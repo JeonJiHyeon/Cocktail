bf77c4e133e7984802cab0d7136c15c5bbcd42e9
JeonJiHyeon <angry_smile@naver.com>
2022-10-17 20:08:51 +0900

    make many buttons in ingredient fragment

--- app/src/main/java/com/navi_baekgu/ui/ingredient/IngredientFragment.java
+++ app/src/main/java/com/navi_baekgu/ui/ingredient/IngredientFragment.java
@@ -27,13 +27,13 @@ public class IngredientFragment extends Fragment {
         binding = FragmentIngredientBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textIngredient;
-        ingredientViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = binding.textIngredient;
+//        ingredientViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 
