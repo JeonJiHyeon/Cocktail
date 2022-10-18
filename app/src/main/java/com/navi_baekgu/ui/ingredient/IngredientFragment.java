4150025636cee88f55e7b513f60006a5476a24b6
JeonJiHyeon <angry_smile@naver.com>
2022-10-18 15:25:14 +0900

    make many buttons and texts in recipe fragment

--- app/src/main/java/com/navi_baekgu/ui/ingredient/IngredientFragment.java
+++ app/src/main/java/com/navi_baekgu/ui/ingredient/IngredientFragment.java
@@ -1,9 +1,13 @@
 package com.navi_baekgu.ui.ingredient;
 
+import android.graphics.Outline;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -27,6 +31,20 @@ public class IngredientFragment extends Fragment {
         binding = FragmentIngredientBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
+
+        final ImageView imageView2 = binding.imageView2;
+
+        ViewOutlineProvider mViewOutlineProvider = new ViewOutlineProvider() {
+            @Override
+            public void getOutline(final View view, final Outline outline) {
+                float cornerRadiusDP = 16f;
+                float cornerRadius = TypedValue.applyDimension( TypedValue.COMPLEX_UNIT_DIP, cornerRadiusDP, getResources().getDisplayMetrics());
+                outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), cornerRadius);
+            }
+        };
+        imageView2.setOutlineProvider(mViewOutlineProvider);
+        imageView2.setClipToOutline(true);
 //        final TextView textView = binding.textIngredient;
 //        ingredientViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
--- app/src/main/java/com/navi_baekgu/ui/recipe/RecipeFragment.java
+++ app/src/main/java/com/navi_baekgu/ui/recipe/RecipeFragment.java
@@ -26,14 +26,6 @@ public class RecipeFragment extends Fragment {
 
         binding = FragmentRecipeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textRecipe;
-        recipeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 
