3d952afe4e94ddf20b144e3e37490386f05621d4
soy-A <skql1502@gmail.com>
2022-11-04 21:08:19 +0900

    feat: Add sample arcore

--- app/src/main/java/com/navi_baekgu/common/helpers/CameraPermissionHelper.java
+++ app/src/main/java/com/navi_baekgu/common/helpers/CameraPermissionHelper.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.navi_baekgu;
+package com.navi_baekgu.common.helpers;
 
 import android.Manifest;
 import android.app.Activity;
